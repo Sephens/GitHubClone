48e98c2ec171f6f0cca1c231c0d5befde2731cf3
Sephens <adesephens@gmail.com>
2022-10-22 22:40:48 +0300

    week 5 of mern

--- gfrontend/src/App.js
+++ gfrontend/src/App.js
@@ -3,18 +3,9 @@ import Navbar from './components/Navbar';
 import Main from './components/Main';
 import Profile from './Pages/Profile'
 import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Footer from './components/Footer'
-import { useState } from 'react';
-import Login from '../src/Pages/Login'
+import Footer from './components/Footer';
 
 function App() {
-  const [token, setToken] = useState();
-  // condition to display Login if the token is false
-  // pass the setToken function to the Login component
-  // check to display Login component on every route if the user is not yet logged in.
-  if(!token){
-    return <Login setToken={setToken}/>
-  }
   return (
     <BrowserRouter>
     <div className="App">
