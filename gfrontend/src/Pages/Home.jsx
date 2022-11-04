cfef4f4d667ad2d803abe0593d7b22f04dbb6be7
Sephens <adesephens@gmail.com>
2022-11-04 15:01:27 +0300

    hey test

---
+++ gfrontend/src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from '../images/logo2.png'
+
+function Home() {
+  return (
+    <div className="container">
+      <div className="nav">
+        <div className="logo">
+        <img src={logo} alt="logo"/>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
