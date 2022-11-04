878b253c6472995edf39a4e413fa5cc8760bfec7
Sephens <adesephens@gmail.com>
2022-11-04 14:15:12 +0300

    more task

--- gfrontend/src/Pages/Login.jsx
+++ gfrontend/src/Pages/Login.jsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 import '../styles/Login.scss'
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import Cookies from 'universal-cookie'
 
 // async function loginUser(credentials){
 //   return fetch('http://localhost:8080/login', {
@@ -20,6 +21,15 @@ function Login() {
   const [password, setPassword] = useState();
   const [login, setLogin] = useState(false);
 
+  const cookies = new Cookies();
+
+const handleSetEmail = (e) => {
+  setEmail(e.target.value)
+}
+
+const handleSetPassword = (e) => {
+  setPassword(e.target.value);
+}
 
 const handleSubmit = (e) => {
   e.preventDefault();
@@ -34,6 +44,14 @@ const handleSubmit = (e) => {
   axios(configuration)
   .then((result)=>{
     setLogin(true);
+    console.log(result);
+
+    // This code above sets the cookie with cookie.set(). It takes three arguments: Name of the cookie (here it's "TOKEN", but it can be anything that you choose), Value of the cookie (result.data.token), and on which page or route you want it to be available (setting the path to "/" makes the cookie available in all the pages).
+    cookies.set("TOKEN", result.data.token,{
+      path: "/",
+    });
+
+    window.location.href("/Profile")
   })
   .catch((er) => {
     er = new Error();
@@ -46,11 +64,12 @@ const handleSubmit = (e) => {
       <form onSubmit={(e)=>handleSubmit(e)}>
         <label>
           <p>Email</p>
-          <input type="text" value={email} placeholder="username" onChange={e => setEmail(e.target.value)}/>
+
+          <input type="text" value={email} placeholder="username" onChange={handleSetEmail}/>
         </label>
         <label>
           <p>Password</p>
-          <input type="password" value={password} placeholder="password" onChange={e => setPassword(e.target.value)}/>
+          <input type="password" value={password} placeholder="password" onChange={handleSetPassword}/>
         </label>
         <div>
           <button type='submit' onClick={(e)=>handleSubmit(e)}>Log In</button>
